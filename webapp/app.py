c50898efa38ced514653764a8d184c19e92a193f
pemcshane <pmcshane@illumina.com>
2014-10-21 12:38:53 -0700

    Update app.py
    Forcing a fail.....

--- webapp/app.py
+++ webapp/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     provider = str(os.environ.get('PROVIDER', 'world'))
-    return 'Hello '+provider+'!'
+    return 'Goodbye cruel '+provider+'!'
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
