795707741ccfe0702f3cdc0d286aa16aec51ea0a
josh-nw <joshua@nebulaworks.com>
2015-11-18 08:03:15 -0600

    Update tests.py

--- webapp/tests.py
+++ webapp/tests.py
@@ -8,7 +8,7 @@ class AppTestCase(unittest.TestCase):
    def test_root_text(self):
         tester = app.test_client(self)
         response = tester.get('/')
-        assert 'Hello world!' in response.data
+        assert 'Hello josh!' in response.data
 
 if __name__ == '__main__':
     unittest.main()
