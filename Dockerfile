e37cbab1cb2094c99aed28dd1ea39fd4b328b75e
Christian Kemy <ckemy@Christians-MacBook-Pro.local>
2022-08-11 02:21:17 -0400

    1st change

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM ubuntu:14.04
-MAINTAINER Docker Education Team <education@docker.com>
+LABEL MAINTAINER Docker Education Team <education@docker.com>
 RUN apt-get update
 RUN DEBIAN_FRONTEND=noninteractive apt-get install -y -q python-all python-pip 
 ADD ./webapp/requirements.txt /tmp/requirements.txt
---
+++ docker_push.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -e
+
+# Declare variable 
+AWS_ACCOUNT_ID=140124015873
+EB_REGION=us-east-1
+DOCKER_REPO=ml-app
+
+
+# the registry should have been created already
+# you could just paste a given url from AWS but I'm
+# parameterising it to make it more obvious how its constructed
+REGISTRY_URL=${AWS_ACCOUNT_ID}.dkr.ecr.${EB_REGION}.amazonaws.com
+
+# this is most likely namespaced repo name like myorg/veryimportantimage
+SOURCE_IMAGE="${DOCKER_REPO}"
+
+# using it as there will be 2 versions published
+TARGET_IMAGE="${REGISTRY_URL}/${DOCKER_REPO}"
+
+# lets make sure we always have access to latest image
+TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
+TIMESTAMP=$(date '+%Y%m%d%H%M%S')
+
+# using datetime as part of a version for versioned image
+VERSION="${TIMESTAMP}-${TRAVIS_COMMIT}"
+
+# using specific version as well
+# it is useful if you want to reference this particular version
+# in additional commands like deployment of new Elasticbeanstalk version
+TARGET_IMAGE_VERSIONED="${TARGET_IMAGE}:${VERSION}"
+
+# making sure correct region is set
+aws configure set default.region ${EB_REGION}
+
+# Push image to ECR
+###################
+
+# I'm speculating it obtains temporary access token
+# it expects aws access key and secret set
+# in environmental vars
+$(aws ecr get-login --no-include-email)
+
+# update latest version
+docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_LATEST}
+docker push ${TARGET_IMAGE_LATEST}
+
+# push new version
+docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_VERSIONED}
+docker push ${TARGET_IMAGE_VERSIONED}
